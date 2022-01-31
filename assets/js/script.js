f40ebcda6e84bd81ae0a2c4594ec0d09aee58261
Emmanuel Lyimo <esal29t@gmail.com>
2022-01-31 01:17:25 -0800

    modified index.html and assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,49 @@ var today = $("#currentDay");
 var hours = [7,8,9,10,11,12,13,14,15,16]
 var localStorageHappening = localStorage.getItem('happening-today');
 var happeningToday;
+var container = $('.container');
 
-if (!localStorageHappening) {
-    happeningToday ={};
-}
-else {
-    happeningToday = JSON.parse(localStorageHappening);
-}
 
-//Displays the current date on the page
 today = moment().format('dddd,MMM Do');
 $("#currentDay").text(today);
+
+for(var i=0; i<24; i++) {
+    var TimeBlock = $("#hr-" + i);
+
+    if (i<moment().hour()){
+        TimeBlock.addClass("past")
+    }
+    if (i==moment().hour()){
+        TimeBlock.addClass("present")
+    }
+    if (i>moment().hour()){
+        TimeBlock.addClass("future")
+    }
+}
+
+// if (!localStorageHappening) {
+//     happeningToday ={};
+// }
+// else {
+//     happeningToday = JSON.parse(localStorageHappening);
+// }
+
+//Displays the current date on the page
+var savedAssignment
+container.on("click", "button", function(event){
+    event.preventDefault()
+    var hour = $(event.target).parent().attr("id")
+    var task = $(event.target).siblings().attr("textarea").val()
+
+    localStorage.setItem(hour,task)
+    console.log(task)
+
+});
+
+
+
+$("#hour-8 textarea").val(localStorage.getItem("happening-today"));
+
 
 
 
