5d21410752a37d149451e29a51ede69fd4e03a50
Emmanuel Lyimo <esal29t@gmail.com>
2022-02-15 23:53:34 -0800

    modified assets

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Daily-Scheduler
 To create a simple calendar application that allows a user to save events for each hour of the day.
+
+
+# Description
+
+This scheduler tool allows the users to save events for each hour of their working day.
+
+# Assets
+
+![wdjJHd.](./assets/images/daily-scheduler-demo.png)
+
+
+
+
+
+
+
+
+
+
+
+# Technologies Used
+HTML,
+CSS,
+JavaScript,
+jQuery,
+Bootstrap,
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var container = $('.container');
 
 $('.saveBtn').on("click",function(){
     var value = $(this).siblings('.description').val();
-    var key =  $(this).parent().attr('id')
+    var key =  $(this).parent().attr('id');
 
-    localStorage.setItem(key, value)
-})
+    localStorage.setItem(key, value);
+});
 
 today = moment().format('dddd,MMM Do');
 $("#currentDay").text(today);
@@ -15,13 +15,13 @@ for(var i=7; i<17; i++) {
     var TimeBlock = $("#hr-" + i);
 
     if (i<moment().hour()){
-        TimeBlock.addClass("past")
+        TimeBlock.addClass("past");
     }
     if (i===moment().hour()){
-        TimeBlock.addClass("present")
+        TimeBlock.addClass("present");
     }
     if (i>moment().hour()){
-        TimeBlock.addClass("future")
+        TimeBlock.addClass("future");
     }
 }
 
@@ -33,15 +33,15 @@ for(var i=7; i<17; i++) {
 
 
 $("#hour-7 textarea").val(localStorage.getItem("hour-7"));
-$("#hour-8 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-9 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-10 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-11 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-12 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-13 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-14 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-15 textarea").val(localStorage.getItem("happening-today"));
-$("#hour-16 textarea").val(localStorage.getItem("happening-today"));
+$("#hour-8 textarea").val(localStorage.getItem("hour-8"));
+$("#hour-9 textarea").val(localStorage.getItem("hour-9"));
+$("#hour-10 textarea").val(localStorage.getItem("hour-10"));
+$("#hour-11 textarea").val(localStorage.getItem("hour-11"));
+$("#hour-12 textarea").val(localStorage.getItem("hour-12"));
+$("#hour-13 textarea").val(localStorage.getItem("hour-13"));
+$("#hour-14 textarea").val(localStorage.getItem("hour-14"));
+$("#hour-15 textarea").val(localStorage.getItem("hour-15"));
+$("#hour-16 textarea").val(localStorage.getItem("hour-16"));
 
 
 
